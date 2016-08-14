9e02ae02c152a1f8b4d5b79d8c21efe180ea0aa0
Sang Park <sanghyup@alumni.cmu.edu>
2016-08-13 17:12:13 -0700

    got docking nav bar

---
+++ new_site/jquerystuff.js
@@ -0,0 +1,13 @@
+$(function(){
+  var navOptions = $(".nav-options");
+  $(window).on('scroll', function() {
+      var yScrollPos = window.pageYOffset;
+      var jumboHeight = 240;
+
+      if(yScrollPos > jumboHeight) {
+        navOptions.addClass("docked");
+      } else if (navOptions.hasClass("docked")){
+        navOptions.removeClass("docked");
+      }
+  });
+});
